539e2c20e94866d1fef332177b3bbfbf9b54565e
QMIHUI <373681997@qq.com>
2020-11-03 15:43:14 +0800

    第二次

---
+++ src/main/java/com/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.controller;
+
+/**
+ * @author HUI
+ * @create 2020-11-03 15:42
+ */
+public class UserController {
+}
---
+++ src/main/java/com/dao/UserDao.java
@@ -0,0 +1,8 @@
+package com.dao;
+
+/**
+ * @author HUI
+ * @create 2020-11-03 15:42
+ */
+public interface UserDao {
+}
