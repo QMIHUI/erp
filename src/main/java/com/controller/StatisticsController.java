eeef63361809d347092be57adc8fcf2d28912312
psqing <623384207@qq.com>
2020-11-05 11:00:39 +0800

    psqing-11/15 11:00

--- src/main/java/com/controller/StatisticsController.java
+++ src/main/java/com/controller/StatisticsController.java
@@ -1,15 +1,14 @@
 package com.controller;
 
 import com.bean.Firm;
-import com.dao.DeptDao;
-import com.dao.FirmDao;
-import com.dao.JobDao;
-import com.dao.UsersDao;
+import com.bean.Purchase;
+import com.dao.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.List;
 
@@ -21,11 +20,20 @@ import java.util.List;
 public class StatisticsController {
     @Autowired
     public FirmDao firmDao;
+    @Autowired
+    public PurchaseDao purchaseDao;
 
     @RequestMapping(value = "getAllFirms.do",method = RequestMethod.GET)
     public String getAllFirms(HttpSession session){
         List<Firm> firmList=firmDao.getAllFrims();
         session.setAttribute("firmList",firmList);
         return "redirect:statis/purchase/purchaseStatis.jsp";
+    }
+    @RequestMapping(value = "getFirmById.do",method = RequestMethod.GET)
+    public String getFirmById(HttpServletRequest request,HttpSession session){
+        int FirmId=Integer.parseInt(request.getParameter("id"));
+        List<Purchase> purchaseList=purchaseDao.getPurchaseByCheckId(FirmId);
+        session.setAttribute("purchaseList",purchaseList);
+        return "redirect:statis/purchase/purchaseView.jsp";
     }
 }
