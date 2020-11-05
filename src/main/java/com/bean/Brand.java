2e1a24b94d3a031549fdd5cd7cd26da661ef0bab
psqing <623384207@qq.com>
2020-11-05 12:01:43 +0800

    psqing-11/15 12:01

---
+++ src/main/java/com/bean/Brand.java
@@ -0,0 +1,88 @@
+package com.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * c_brand
+ * @author 
+ */
+public class Brand implements Serializable {
+    /**
+     * 品牌id
+     */
+    private Integer brandId;
+
+    /**
+     * 品牌名称
+     */
+    private String brandName;
+
+    /**
+     * 商品状态，1可用 2不可用
+     */
+    private Integer brandStatus;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 创建人
+     */
+    private Users creater;
+
+    private static final long serialVersionUID = 1L;
+
+    public Brand() {
+    }
+
+    public Brand(Integer brandId, String brandName, Integer brandStatus, Date createTime, Users creater) {
+        this.brandId = brandId;
+        this.brandName = brandName;
+        this.brandStatus = brandStatus;
+        this.createTime = createTime;
+        this.creater = creater;
+    }
+
+    public Integer getBrandId() {
+        return brandId;
+    }
+
+    public void setBrandId(Integer brandId) {
+        this.brandId = brandId;
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public Integer getBrandStatus() {
+        return brandStatus;
+    }
+
+    public void setBrandStatus(Integer brandStatus) {
+        this.brandStatus = brandStatus;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Users getCreater() {
+        return creater;
+    }
+
+    public void setCreater(Users creater) {
+        this.creater = creater;
+    }
+}
--- src/main/java/com/bean/Type.java
+++ src/main/java/com/bean/Type.java
@@ -36,9 +36,67 @@ public class Type implements Serializable {
     /**
      * 品牌id、外键
      */
-    private Integer brandId;
+    private Brand brand;
 
     private static final long serialVersionUID = 1L;
 
+    public Type() {
+    }
 
+    public Type(Integer typeId, String typeName, Integer typeStatus, Date createTime, Users creater, Brand brand) {
+        this.typeId = typeId;
+        this.typeName = typeName;
+        this.typeStatus = typeStatus;
+        this.createTime = createTime;
+        this.creater = creater;
+        this.brand = brand;
+    }
+
+    public Integer getTypeId() {
+        return typeId;
+    }
+
+    public void setTypeId(Integer typeId) {
+        this.typeId = typeId;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public Integer getTypeStatus() {
+        return typeStatus;
+    }
+
+    public void setTypeStatus(Integer typeStatus) {
+        this.typeStatus = typeStatus;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Users getCreater() {
+        return creater;
+    }
+
+    public void setCreater(Users creater) {
+        this.creater = creater;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
 }
