ec4002a3c2e171c8c0fdd1db9d0a8aef9e8848fe
psqing <623384207@qq.com>
2020-11-16 14:52:55 +0800

    psqing-11/16 14:52

--- src/main/java/com/dao/DetailsDao.java
+++ src/main/java/com/dao/DetailsDao.java
@@ -6,8 +6,9 @@ import java.util.List;
 
 public interface DetailsDao {
     public List<Details> getDetailsByPurchaseId(String purchaseId);
-    //增加订购单详情
+    //增加采购单详情
     public int addPurchaseDetails(String purchaseId,int count,int productId,double productPrice,double productTotalMoney);
-    //修改订购单详情
-    public int updatePurchaseDetails(int count,int productId,double productPrice,double productTotalMoney,int detailsId);
+
+    //根据采购单号删除采购单详情
+    public int delDetails(String purchaseId);
 }
